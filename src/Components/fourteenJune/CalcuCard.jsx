e75adb4fb4850ffd8664aff80047ccb454049de0
Jahanzaib <m.jahanzaib727@gmail.com>
2021-06-14 16:49:55 +0500

    repalce with better design

--- src/Components/fourteenJune/CalcuCard.jsx
+++ src/Components/fourteenJune/CalcuCard.jsx
@@ -8,27 +8,27 @@ export default function CalcuCard() {
     return (
         <div className={styles.calcucardWrapper} style={{
             backgroundImage: "url('/images/backofcalcu.svg')",
-            backgroundSize: "contain",
+            backgroundSize: "cover",
             backgroundRepeat: "no-repeat",
             backgroundColor: '#edfaf2',
         }}>
             <h6 className={styles.statement}><span><FcCalculator className={styles.calc} /></span> Investment Calculator</h6>
-            <section className={styles.calcucard}>
-                <div className={styles.calcuHeader}>
+            <table className={styles.calcucard}>
+                <thead className={styles.calcuHeader}>
                     <p>Investment Amount</p>
                     <div><CalcuInput /></div>
-                </div>
-                <div className={styles.cardBottom}>
-                    <div className={styles.cardBottomDiv1}>
+                </thead>
+                <tbody className={styles.cardBottom}>
+                    <tr className={styles.cardBottomDiv1}>
                         <p className={styles.label}>Duration</p>
                         <p className={styles.data}>4 Months</p>
-                    </div>
-                    <div className={styles.cardBottomDiv2}>
+                    </tr>
+                    <tr className={styles.cardBottomDiv2}>
                         <p className={styles.label}>Monthly Rent</p>
                         <p className={styles.data}>300,000 pkr/-</p>
-                    </div>
-                </div>
-            </section>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
